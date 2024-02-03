ea153511282293f7beecb1d2a47689d11a34730a
sumitag-25 <156592907+sumitag-25@users.noreply.github.com>
2024-02-03 17:38:34 +0000

    commit

---
+++ conversion/new.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+let valueOfScore = Number(score)
+
+// console.log(typeof valueOfScore)
+// console.log(valueOfScore)
+
+let vale = 0
+
+let valueOfValue = Boolean(vale)
+
+console.log(typeof valueOfValue)
+console.log(valueOfValue)
+
---
+++ operations/new.js
@@ -0,0 +1,21 @@
+// operations
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 * 3)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+
+// let firstName = "Sumit"
+// let lastName = " Verma"
+
+// let fullName = firstName + lastName
+// console.log(fullName);
+
+
+console.log( 1 + "2")
+console.log( "1" + 2)
+console.log( 1 + 1 + "2")
+console.log( "1" + 2 + 1)
